95a98a70368c80ac30bbc7960c5415a970511e2d
Keegan <keegs4213@gmail.com>
2024-03-30 11:52:55 +1300

    fixed viewheight

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-<div className="flex justify-center items-center text-center" style={{ height: '83vh' }}>
+<div className="flex justify-center items-center text-center" style={{ height: '120vh' }}>
       
       <div>
         <h1 className="text-5xl mb-4" id="h1">
@@ -28,10 +28,11 @@ export default function Hero() {
             height={300}
             className="codingCartoon"
           ></Image>
-        </div>
-        <div>
           
-          <a href="https://drive.google.com/file/d/1i65f5mxWnH4cwLJM40DhzYVbngaf3F2P/view" target="_blank">
+        </div>
+        
+        <div>
+        <a href="https://drive.google.com/file/d/1i65f5mxWnH4cwLJM40DhzYVbngaf3F2P/view" target="_blank">
           <button class="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-500 rounded-full">
   Check my Resume
 </button>
@@ -42,6 +43,7 @@ export default function Hero() {
   Linked in
 </button>
           </a>
+          
         </div>
       </div>
     </div>
